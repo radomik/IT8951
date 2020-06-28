5f16f015ea4020ed7060cfd762d6fec70381778b
Dariusz Mikołajczuk <radomik@gmail.com>
2020-06-29 00:51:41 +0200

    Redraw only if screenshot have changed

--- run_scrot.sh
+++ run_scrot.sh
@@ -10,6 +10,19 @@ fi
 
 while [ true ] ; do 
 	scrot "$IMG_PATH"
+	if [ ! -f "$IMG_PATH" ] ; then
+		echo "Fail to take screenshot to $IMG_PATH"
+		exit 1
+	fi
+	if [ -f "$IMG_PREV_PATH" ] ; then
+		SUM1=`md5sum -b "$IMG_PATH" | cut -d' ' -f1`
+		SUM2=`md5sum -b "$IMG_PREV_PATH" | cut -d' ' -f1`
+		if [ "$SUM1" == "$SUM2" ] ; then
+			sleep 0.05
+			continue
+		fi
+	fi
+	cp "$IMG_PATH" "$IMG_PREV_PATH"
 	sudo kill -s USR1 "$1"
 	while [ -f "$IMG_PATH" ] ; do 
 		sleep 0.05
--- vars.sh
+++ vars.sh
@@ -2,5 +2,7 @@
 
 #fstab: vartmp  /mnt/ramtmp  tmpfs rw,mode=1777  0 0
 IMG_PATH="/mnt/ramtmp/out.bmp"
+IMG_PREV_PATH="/mnt/ramtmp/out_prev.bmp"
+
 
 
